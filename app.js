ecfe4dc090f260f4c7922682417b82b0225e8231
alecvarma <alec_varma@yahoo.com>
2022-06-26 21:04:37 -0400

    Added Icon Links
    - Added Icon Links to Email, LinkedIn, GitHub, Resume
    - Added similar animation as name to "fade in" the icons (buttons cannot be pressed before animation is finished)
    - Commented out javascript function to find each letter's stroke length since it's unneeded currently
    - Used FontAwesome5 for icons

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const logo = document.querySelectorAll("#logo path");
-for(let i = 0; i<logo.length; i++) {
-    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`)
-}
+// const logo = document.querySelectorAll("#logo path");
+// for(let i = 0; i<logo.length; i++) {
+//     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`)
+// }
+
+
